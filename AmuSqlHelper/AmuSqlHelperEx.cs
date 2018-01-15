804058bb65b5bd79583273313451036a1ce1cb56
liumingy <648181379@qq.com>
2018-01-15 17:09:59 +0800

    解决插入数据后返回不能设置id的bug

--- AmuSqlHelper/AmuSqlHelperEx.cs
+++ AmuSqlHelper/AmuSqlHelperEx.cs
@@ -174,7 +174,7 @@ namespace AmuTools
                 values += "'" + temp_value + "'" + (notLast ? "," : "");
             }
             string sql_str = string.Format("insert into [{0}] ({1}) values ({2});select @@IDENTITY", pm.Model.TableName, columns, values);
-            SqlResult sr = Set(sql_str);
+            SqlResult sr = Get(sql_str);
             if (pm.Model.IdentityInsert)
             {
                 if ((pi = type.GetProperty(pm.Model.PrimaryKey)) != null)
@@ -182,7 +182,7 @@ namespace AmuTools
                     pi.SetValue(obj, Convert.ChangeType(sr.ScalarValue, pi.PropertyType));
                 }
             }
-            return sr.EffectedLineCount;
+            return 1;
         }
         public int Insert<T>(T obj) where T : class, new()
         {
